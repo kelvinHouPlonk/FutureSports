50dfccbca97233e4b4a00997e2030a9a7f955e30
Kelvin Hou <khout1@hotmail.my>
2023-09-20 22:13:09 +1000

    first try to put on domain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './App.css';
-import Navigation from "./Components/Navigation/Nav";
-import Header from "./Components/Header/Header";
-import Main from "./Components/Main/Main";
-import Aside from "./Components/Aside/UpComingMatches";
-import Footer from "./Components/Footer/Copyright";
-import Section1 from "./Components/Section1/Section1";
-import Section2 from "./Components/Section2/Gallery";
+import Navigation from './Components/Navigation/Nav';
+import Main from './Components/Main/Main';
+import Aside from './Components/Aside/UpComingMatches';
+import Footer from './Components/Footer/Copyright';
+import Section1 from './Components/Section1/Section1';
+import Section2 from './Components/Section2/Gallery';
+import ErrorBoundary from './Components/Error/ErrorBoundary';
+import Weather from './Components/Weather/Weather';
+import MatchHistory from './Components/Header/MatchHistory';
+
+function Header() {
+  return (
+    <div className='Welcome'>
+      <h1>Welcome To FutureSports</h1>
+      <br />
+      <Weather />
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-        <Navigation />
-        <Header />
-        <Main />
-        <Aside />
-        <Section2 />
-        <Section1 />
-        <Footer />
+      <Navigation />
+      <Header />
+      <MatchHistory />
+      <Main />
+      <Aside />
+      <Section2 />
+      <Section1 />
+      <Footer />
     </div>
   );
 }
